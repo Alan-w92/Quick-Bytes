45c758edd4546581f07cadba5d4f32e747fdca5a
Alan W <alanw2129@gmail.com>
2017-08-01 22:18:01 -0400

    react config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,13 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h1>Boo Yaa</h1>,
+    document.getElementById('app')
+  );
+});
