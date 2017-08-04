a2c34684ca79d48600720835fd5228dc88317e0f
Alan W <alanw2129@gmail.com>
2017-08-03 22:43:35 -0400

    add list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,26 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
+  end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      flash[:alert] = "List added successfully"
+      redirect_to list_path(@list)
+    else
+      flash[:errors] = @list.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:comment, :recipe_name, :ingredient, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @search = params['search']
   end
+
+  def show
+
+  end
 end
