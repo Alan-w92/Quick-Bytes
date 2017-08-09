28b22a5c7a149985916166a72f8c9c24cfe562d3
Alan W <alanw2129@gmail.com>
2017-08-09 15:26:39 -0400

    add-css

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe List, type: :model do
 
   it { should have_valid(:recipe_name).when('Chicken Pizza', '30 min Tomato Soup') }
   it { should_not have_valid(:recipe_name).when('', nil) }
+
+  it { should have_valid(:ingredient).when('3 lbs Pasta', '3 oz pepper', 'bread') }
+  it { should_not have_valid(:ingredient).when('', nil) }
 end
