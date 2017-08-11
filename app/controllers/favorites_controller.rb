7d6b95dc29ec1c8d096fe0b05d02556ade10e1b5
Alan W <alanw2129@gmail.com>
2017-08-11 14:25:30 -0400

    favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,15 +10,15 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.new(favorite_params)
     if @favorite.save
-      flash[:alert] = "Recipe added to your favorites."
-      redirect_to users_path
+      flash[:alert] = "Recipe added to your favorites"
+      redirect_to favorites_path
     end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    redirect_to favorites_path, notice: 'Favorite recipe was successfully removed.'
+    redirect_to favorites_path, notice: 'Favorite recipe was successfully removed'
   end
 
   private
