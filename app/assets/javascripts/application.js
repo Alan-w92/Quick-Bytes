51964bb3261bc4e7ab1e91949b9cd178a23c7b2c
Alan W <alanw2129@gmail.com>
2017-08-11 18:27:49 -0400

    initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,13 +17,3 @@
 //= require foundation
 
 $(function(){ $(document).foundation(); });
-
-$(document).ready(function(){
-  $(".option").hide();
-
-  $([10,11,12,13,14,15,16,17,18,19]).each(function(i, e){
-    $(`#${i}`).click(function(){
-      $(`#${e}`).slideToggle("slow");
-    });
-  });
-});
