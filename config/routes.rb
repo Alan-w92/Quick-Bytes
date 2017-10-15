c364ab513cf61297c2257b26623eb9f0c094751e
Alan W <alanw2129@gmail.com>
2017-10-14 21:09:33 -0400

    google-map-view

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index]
+  resources :favorites
+  resources :maps, only: [:index]
   resources :lists do
     resources :comments, only: [:new, :create, :update, :edit, :destroy]
   end
-  resources :favorites
 end
